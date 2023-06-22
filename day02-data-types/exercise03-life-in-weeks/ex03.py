6850726a356ddae2e2b4ffba9e7cf262cf9290c1
migenjutsu <domhenin.517@gmail.com>
2023-06-21 21:06:45 -0400

    still wip :: working on project

--- day02-data-types/exercise03-life-in-weeks/ex03.py
+++ day02-data-types/exercise03-life-in-weeks/ex03.py
@@ -8,3 +8,11 @@ year = 365
 week = 52
 month = 12
 
+age_int = int(age)
+
+remain_year = 90 - age_int
+remain_day = remain_year * 365
+remain_week = remain_year * 52
+remain_month = remain_year * 12
+
+print(f"You have {remain_day} days, {remain_week} weeks, and {remain_month} months left.")
