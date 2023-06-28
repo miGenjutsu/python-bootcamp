3d14cfc52fc31fb6336d65df50930666138cd811
clxdev <domhenin.517@gmail.com>
2023-06-28 10:41:53 -0400

    completed exercise3.5

--- day03-control-flow-logical-operations/exercise05-love-calculator/ex3-5.py
+++ day03-control-flow-logical-operations/exercise05-love-calculator/ex3-5.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 # 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
@@ -8,76 +6,74 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
-score = 0
 
-name1_lower = name1.upper()
-name2_lower = name2.upper()
+# name1_lower = name1.upper()
+# name2_lower = name2.upper()
 
-total_count_1 = (
-    name1_lower.count('T')+
-    name1_lower.count('R')+
-    name1_lower.count('U')+
-    name1_lower.count('E')
-    # name1_lower.count('L')+
-    # name1_lower.count('O')+
-    # name1_lower.count('V')+
-    # name1_lower.count('E')
-)
+combine_names = name1 + name2
+lower_case_string = combine_names.lower()
 
-total_count_1 = (
-    # name1_lower.count('T')+
-    # name1_lower.count('R')+
-    # name1_lower.count('U')+
-    # name1_lower.count('E')
-    name1_lower.count('L')+
-    name1_lower.count('O')+
-    name1_lower.count('V')+
-    name1_lower.count('E')
-)
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
 
-# result_1 = print(str(total_count_1))
-# result_1 = total_count_1
-result_1 = print(str(total_count_1))
+true = t + r + u + e
 
-total_count_2 = (
-    name2_lower.count('T')+
-    name2_lower.count('R')+
-    name2_lower.count('U')+
-    name2_lower.count('E')
-    # name2_lower.count('L')+
-    # name2_lower.count('O')+
-    # name2_lower.count('V')+
-    # name2_lower.count('E')
-)
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
 
-total_count_2 = (
-    # name2_lower.count('T')+
-    # name2_lower.count('R')+
-    # name2_lower.count('U')+
-    # name2_lower.count('E')+
-    name2_lower.count('L')+
-    name2_lower.count('O')+
-    name2_lower.count('V')+
-    name2_lower.count('E')
-)
-
-result_2 =  print(str(total_count_2))
+love = l + o + v + e
 
 
-print(f"The number of letters for NAME 1 is: {total_count_1}")
-print(f"The number of letters for NAME 2 is: {total_count_2}")
+love_score = int(str(true + love))
 
-if total_count_1 > total_count_2:
-    # print(total_count_1 + total_count_2)
-    score = str(total_count_1) + str(total_count_2)
+if love_score <= 10 or love_score >= 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif score >= 40 and score <=50:
+    print(f"Your score is {love_score}, you are alright together.")
 else:
-    score = str(total_count_2) + str(total_count_1)
+    print(f"Your score is {love_score}.")
+
+
+# total_count_1 = (
+#     name1_lower.count('T')+
+#     name1_lower.count('R')+
+#     name1_lower.count('U')+
+#     name1_lower.count('E')
+#     # name1_lower.count('L')+
+#     # name1_lower.count('O')+
+#     # name1_lower.count('V')+
+#     # name1_lower.count('E')
+# )
+
+# # result_1 = print(str(total_count_1))
+# # result_1 = total_count_1
+# result_1 = print(str(total_count_1))
+
+# total_count_2 = (
+#     name2_lower.count('T')+
+#     name2_lower.count('R')+
+#     name2_lower.count('U')+
+#     name2_lower.count('E')
+#     # name2_lower.count('L')+
+#     # name2_lower.count('O')+
+#     # name2_lower.count('V')+
+#     # name2_lower.count('E')
+# )
+
+# print(f"The number of letters for NAME 1 is: {total_count_1}")
+# print(f"The number of letters for NAME 2 is: {total_count_2}")
+
+# if total_count_1 > total_count_2:
+#     # print(total_count_1 + total_count_2)
+#     score = str(total_count_1) + str(total_count_2)
+# else:
+#     score = str(total_count_2) + str(total_count_1)
 
 # score = str(total_count_1) + str(total_count_2)
-
-print(score)
-
-
 
 # if score <= 10 and score >= 90:
 #     print(f"Your score is {score}, you go together like coke and mentos.")
