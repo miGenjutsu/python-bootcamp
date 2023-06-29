4b4f7a2bdb01c112a2ae92208dc0559b5e998218
clxdev <domhenin.517@gmail.com>
2023-06-29 14:25:26 -0400

    completed exercise4.3

---
+++ day04-randomisation-python-lists/exercise03-treasure-map/extest.py
@@ -0,0 +1,25 @@
+# PURPOSE:: Testing different fucntionings for the desired outcome
+
+# my_list = [["A", "B"], ["C", "D"]]
+# replacement = "X"
+# nested_index = 0
+# element_index = 1
+
+# my_list[nested_index][element_index] = replacement
+# print(my_list)
+
+
+## splitting user input test
+# user_input = input("Enter two integers separated by a space: ")
+# numbers = user_input.split()
+
+# if len(numbers) == 2:
+#     try:
+#         num1 = int(numbers[0])
+#         num2 = int(numbers[1])
+#         print("First number:", num1)
+#         print("Second number:", num2)
+#     except ValueError:
+#         print("Invalid input. Please enter two integers.")
+# else:
+#     print("Invalid input. Please enter two integers separated by a space.")
