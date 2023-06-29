8c8f9db6e13ea0b4bfd41397f5759a0e9c94bf82
clxdev <domhenin.517@gmail.com>
2023-06-29 14:46:15 -0400

    completed exercise4.3

--- day04-randomisation-python-lists/exercise03-treasure-map/exer03_module.py
+++ day04-randomisation-python-lists/exercise03-treasure-map/exer03_module.py
@@ -1,29 +1,17 @@
-# # 🚨 Don't change the code below 👇
-# # row1 = ["⬜️","️⬜️","️⬜️"]
-# # row2 = ["⬜️","⬜️","️⬜️"]
-# # row3 = ["⬜️️","⬜️️","⬜️️"]
-# row1 = ["🍎","️🍐","️🍊"]
-# row2 = ["🍔","🍱","️🍕"]
-# row3 = ["🍨️","🥧️","🍩️"]
-# map = [row1, row2, row3]
-# print(f"{row1}\n{row2}\n{row3}")
-# position = input("Where do you want to put the treasure? ")
-# # 🚨 Don't change the code above 👆
+# 🚨 Don't change the code below 👇
+# row1 = ["⬜️","️⬜️","️⬜️"]
+# row2 = ["⬜️","⬜️","️⬜️"]
+# row3 = ["⬜️️","⬜️️","⬜️️"]
+row1 = ["🍎","️🍐","️🍊"]
+row2 = ["🍔","🍱","️🍕"]
+row3 = ["🍨️","🥧️","🍩️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
 def map_x():
-    # 🚨 Don't change the code below 👇
-    row1 = ["⬜️","️⬜️","️⬜️"]
-    row2 = ["⬜️","⬜️","️⬜️"]
-    row3 = ["⬜️️","⬜️️","⬜️️"]
-    # row1 = ["🍎","️🍐","️🍊"]
-    # row2 = ["🍔","🍱","️🍕"]
-    # row3 = ["🍨️","🥧️","🍩️"]
-    map = [row1, row2, row3]
-    print(f"{row1}\n{row2}\n{row3}")
-    position = input("Where do you want to put the treasure? ")
-    # 🚨 Don't change the code above 👆
-
     split_posistion = position.split() # splitting the position for indexing
     # nested_index = 1
     # element_index = 2
@@ -33,17 +21,13 @@ def map_x():
     nested_index  = int(position[0])
     element_index = int(position[1])
 
+    # Testing the split() function
     # print(f"Nested Index: {nested_index}")
     # print(f"Element Index: {element_index}")
 
     # map[nested_index - 1][element_index - 1] = new_object
-    map[element_index - 1][nested_index - 1] = new_object
-    # print(f"\nNew Food Map:{map}")
-
-    # print(f"Your Desired Position: {position}")
-
+    map[element_index - 1][nested_index - 1] = new_object    
     #Write your code above this row 👆
-
     # 🚨 Don't change the code below 👇
     print(f"{row1}\n{row2}\n{row3}")
 
