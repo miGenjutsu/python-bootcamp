9118f8eee5895bb47c318911bed01a40cf5089a5
migenjutsu <domhenin.517@gmail.com>
2023-06-28 20:48:46 -0400

    exercise4.2 complete

--- day04-randomisation-python-lists/exercise02-banker-roulette/ex4-2.py
+++ day04-randomisation-python-lists/exercise02-banker-roulette/ex4-2.py
@@ -1,18 +1,6 @@
-import random
+import exer02_module
 
-# Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
-# 🚨 Don't change the code above 👆
+exer02_module.random_name_call()
 
-#Write your code below this line 👇
-
-print(names)
-
-return_names = len(names)
-
-# print(return_names)
-
-return_names.random.randint()
-
-print(return_names)
+# another example of the challenge but using the choice() module instead
+# exer02_module.choice_name_call()
