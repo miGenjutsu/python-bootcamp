8c0c5650b3f175378d87df6978b38e70433e70dd
migenjutsu <domhenin.517@gmail.com>
2023-07-07 09:54:37 -0400

    still wip :: project 4

--- day04-randomisation-python-lists/day04-project-rock-paper-scissors/day04-project.py
+++ day04-randomisation-python-lists/day04-project-rock-paper-scissors/day04-project.py
@@ -1,47 +1,67 @@
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+import random
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+rock='''✊'''
+paper='''✋'''
+scissors='''✌️'''
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+# rock = '''
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# '''
+
+# paper = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+# '''
+
+# scissors = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# '''
 
 #Write your code below this line 👇
 
-user_input = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+# get user input logic
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 
-# print(type(scissors))
+# create variable for randomize
+compute_random = [0, 1, 2]
 
-print(type(user_input))
+# if else statements - user input
+if user_input == 0:
+    user_input = rock
+    print(f"Your option: {user_input}")
+elif user_input == 1:
+    user_input = paper
+    print(f"Your option: {user_input}")
+elif user_input == 2:
+    user_input = scissors
+    print(f"Your option: {user_input}")
+else:
+    print("something went wrong")
+  
+random_option = random.choice(compute_random)
 
-
-# if user_input == 0:
-#     user_input == rock
-#     print(user_input)
-# elif user_input == 1:
-#     user_input == paper
-#     print(user_input)
-# elif user_input == 2:
-#     user_input == scissors
-#     print(user_input)
-# else:
-#     print("something went wrong")
+if random_option == 0:
+    random_option = rock
+    print(f"Computer option: {random_option}")
+elif random_option == 1:
+    random_option = paper
+    print(f"Computer option: {random_option}")
+elif random_option == 2:
+    random_option = scissors
+    print(f"Computer option: {random_option}")
+else:
+    print("something went wrong")
