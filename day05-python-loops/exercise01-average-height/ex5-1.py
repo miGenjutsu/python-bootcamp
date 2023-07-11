bb229f693916add6b754081fe131fb0a0e23b036
clxdev <domhenin.517@gmail.com>
2023-07-11 12:09:32 -0400

    still wip :: figuring out dividing logic

--- day05-python-loops/exercise01-average-height/ex5-1.py
+++ day05-python-loops/exercise01-average-height/ex5-1.py
@@ -1,23 +1,3 @@
-# 🚨 Don't change the code below 👇
-student_heights = input("Input a list of student heights ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
+import exer01_module
 
-
-#Write your code below this row 👇
-
-# testing: the count of list
-# print(n)
-# print(student_heights)
-
-total = 0
-
-for height in student_heights:
-    total += height
-    
-print(total)
-
-fin_calcu = total / n
-
-print(round(fin_calcu))
+exer01_module.average_height()
