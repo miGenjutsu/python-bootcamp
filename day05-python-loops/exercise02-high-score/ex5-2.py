fe9770f9cc0a3e8a32d1fa78740fe5d856fb4a76
clxdev <domhenin.517@gmail.com>
2023-07-25 14:31:53 -0400

    completed exercise

---
+++ day05-python-loops/exercise02-high-score/ex5-2.py
@@ -0,0 +1,16 @@
+import exer02_module
+# exer02_module.high_score_simple_solution()
+
+exer02_module.high_score_solution()
+
+# 🚨 Don't change the code below 👇
+# student_scores = input("Input a list of student scores ").split()
+# for n in range(0, len(student_scores)):
+#     student_scores[n] = int(student_scores[n])
+# print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+# print(f"Max Student Score: {max(student_scores)}")
+# print(f"Min Student Score: {min(student_scores)}")
