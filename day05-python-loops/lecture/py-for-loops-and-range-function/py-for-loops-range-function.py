dc58fd7303478cd6a469746243c1b997c19bee57
clxdev <domhenin.517@gmail.com>
2023-07-26 14:07:19 -0400

    finished for-loop range lecture

---
+++ day05-python-loops/lecture/py-for-loops-and-range-function/py-for-loops-range-function.py
@@ -0,0 +1,12 @@
+#For Loop with Range
+# for number in range(1, 11, 3);
+#   print(number)
+
+#For Loop with RANGE
+# for number in range(1, 11, 3):  # ---> prints out 1-10 :: the 3 initate the step size
+#     print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number    
+print(total)
